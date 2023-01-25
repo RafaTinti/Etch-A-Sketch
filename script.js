be3d869d98d3bc9063da44c6f37127cf0cb0eb8f
Rafael Tinti <fael145@gmail.com>
2023-01-25 15:20:50 -0300

    Adds color and rainbow modes

--- script.js
+++ script.js
@@ -1,18 +1,79 @@
 const container = document.querySelector(".container");
-createGrid();
-const squares = document.querySelectorAll("square");
-console.log(squares);
+createGrid(64);
 
+let mode = 0 // controls the mode of the app 0 - color 1 - rainbow 2 - gadient starts in color mode
 
-function createGrid(){
-    for (let i = 0; i<16; i++){
+let squares = document.querySelectorAll(".square");
+const resetBtn = document.querySelector("#reset");
+const resizeBtn = document.querySelector("#resize");
+const colorBtn = document.querySelector("#color");
+const rainbowBtn = document.querySelector("#rainbow");
+const gradientBtn = document.querySelector("#gradient")
+
+resetBtn.addEventListener("click", reset);
+resizeBtn.addEventListener("click", resize);
+colorBtn.addEventListener("click", () => mode = 0);
+rainbowBtn.addEventListener("click", () => mode = 1); 
+gradientBtn.addEventListener("click", () => mode = 2);
+squares.forEach(element => {
+    element.addEventListener("mouseover", paintSquare);  
+});
+
+function paintSquare(e){
+    //if (e.buttons === 0) return;
+    switch(mode){
+        case 0:
+            this.classList.add("painted");
+            break;
+        case 1:
+            this.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
+            console.log(`rgb(${randomColor()},${randomColor()},${randomColor()})`);
+            break;
+        case 2:
+            console.log("2");
+            break;
+        default:
+            console.log("mode is invalid");
+    }
+}
+
+function createGrid(size){
+    for (let i = 0; i<size; i++){
         const col = document.createElement("div");
         col.classList.add("column");
-        for (let j = 0; j<16; j++){
+        for (let j = 0; j<size; j++){
             const div = document.createElement("div");
             div.classList.add("square");
             col.appendChild(div);
         }
         container.appendChild(col);
     }
+}
+
+function reset(e){
+    squares.forEach(element => {
+        element.classList.remove("painted");
+        element.style.backgroundColor = "white";
+    });
+}
+
+function resize(e){
+    answer = prompt("what is the new size?");
+    if(Number.isInteger(+answer) && answer<100){
+        while(container.firstChild){
+            container.removeChild(container.firstChild);
+        }
+        createGrid(answer);
+        squares = document.querySelectorAll(".square");
+        squares.forEach(element => {
+            element.addEventListener("mouseover", paintSquare);  
+        });
+    }
+    else{
+        alert("invalid");
+    }
+}
+
+function randomColor(){
+    return Math.floor(Math.random()*256);
 }
